f20f9de489d34d5342bfa177942729f0be6a7413
kferles <kferles@gmail.com>
2014-05-17 13:49:55 +0300

    Adding program arguments.

--- src/server/CcmServer.java
+++ src/server/CcmServer.java
@@ -1,11 +1,14 @@
 package server;
 
+import config.ConfigParameters;
+import deadlockmanager.DeadlockManager;
 import exception.InvalidKeyFactoryException;
 import exception.InvalidRecordSize;
 import file.index.BPlusIndex;
 import file.record.sample.EmployeeFactory;
 import file.record.sample.EmployeeKeyValFactory;
 import file.record.sample.EmployeeRecord;
+import util.FileSystemMethods;
 
 import java.io.IOException;
 import java.net.ServerSocket;
@@ -13,16 +16,13 @@ import java.net.Socket;
 
 public class CcmServer implements Runnable {
 
-    private final int port;
-
     private BPlusIndex<Integer, EmployeeRecord> index;
 
     private final ServerSocket socket;
 
-    private volatile boolean stop = false;
+    public static volatile boolean stop = false;
 
     public CcmServer(int port, BPlusIndex<Integer, EmployeeRecord> index) throws IOException {
-        this.port = port;
         this.socket = new ServerSocket(port);
         this.index = index;
     }
@@ -48,12 +48,85 @@ public class CcmServer implements Runnable {
 
     public static void main(String[] args){
         try {
+            String recordFile = null;
+            int bufferSize = 1024;
+            int buffersNum = 500;
+            int port = 2345;
+
+            ConfigParameters configParameters = ConfigParameters.getInstance();
+
+            for(int i = 0; i < args.length; ++i){
+                String arg = args[i];
+                switch(arg){
+                    case "-f":
+                        recordFile = args[++i];
+                        break;
+                    case "-bs":
+                        bufferSize = Integer.parseInt(args[++i]);
+                        break;
+                    case "-bn":
+                        buffersNum = Integer.parseInt(args[++i]);
+                        break;
+                    case "-p":
+                        port = Integer.parseInt(args[++i]);
+                        break;
+                    case "-d":
+                        configParameters.setDurable(true);
+                        break;
+                    default:
+                        System.err.println(args[i] + ": invalid argument");
+                        System.exit(1);
+                }
+            }
+
+            if(recordFile == null){
+                System.err.println("missing record file");
+                System.exit(1);
+            }
+
+            configParameters.setBufferSize(bufferSize);
+            configParameters.setMaxBuffNumber(buffersNum);
+
+            boolean initializeIndex = !FileSystemMethods.exists(FileSystemMethods.getPath(recordFile));
+
             EmployeeFactory recFac = new EmployeeFactory();
             EmployeeKeyValFactory keyFac = new EmployeeKeyValFactory();
-            BPlusIndex<Integer, EmployeeRecord> index = new BPlusIndex<>("first_test", false, recFac, keyFac);
-            CcmServer server = new CcmServer(2345, index);
+            BPlusIndex<Integer, EmployeeRecord> index = new BPlusIndex<>(recordFile, initializeIndex, recFac, keyFac);
+
+            final CcmServer server = new CcmServer(port, index);
+            final Thread mainThread = Thread.currentThread();
+            final Thread deadlockManagerThread = new Thread(new DeadlockManager());
+
+            Runtime.getRuntime().addShutdownHook(new Thread(){
+                @Override
+                public void run(){
+                    try {
+                        System.out.println("Running shutdown hook");
+                        System.out.flush();
+                        stop = true;
+                        server.socket.close();
+                        try {
+                            mainThread.join();
+                        } catch (InterruptedException e) {
+                            mainThread.interrupt();
+                        }
+
+                        try {
+                            deadlockManagerThread.join();
+                        } catch (InterruptedException e) {
+                            deadlockManagerThread.interrupt();
+                        }
+                    }
+                    catch (IOException e){
+                        e.printStackTrace();
+                    }
+                }
+            });
+
+            deadlockManagerThread.start();
             server.run();
-        } catch (Exception e) {
+        }
+        catch(Exception e){
             System.err.println("failed to start server: " + e.getMessage());
         }
     }

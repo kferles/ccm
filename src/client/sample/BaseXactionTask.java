2b16c24675433f5788d88c90e38e192d14ac638d
kferles <kferles@gmail.com>
2014-05-18 21:38:16 +0300

    Bulk insert and delete operations (thanks to Katerina).

--- src/client/sample/BaseXactionTask.java
+++ src/client/sample/BaseXactionTask.java
@@ -7,9 +7,12 @@ import exception.InvalidRecordSize;
 import exception.RemoteFailure;
 import file.record.sample.EmployeeRecord;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
 import java.net.Socket;
 import java.util.ArrayList;
+import java.util.StringTokenizer;
 
 public abstract class BaseXactionTask implements XactionTask{
 
@@ -21,6 +24,41 @@ public abstract class BaseXactionTask implements XactionTask{
 
     protected void endXaction() throws RemoteFailure, IOException, ClassNotFoundException {
         dispatcher.endXaction();
+    }
+
+    protected void loadFromFile(String filename) throws ClassNotFoundException, InvalidRecordException, RemoteFailure {
+
+        try(BufferedReader reader = new BufferedReader(new FileReader(filename))){
+            String line;
+            while ((line = reader.readLine()) != null) {
+                StringTokenizer st = new StringTokenizer(line, ",");
+
+                int id = Integer.parseInt(st.nextToken());
+                String firstName = st.nextToken();
+                String lastName = st.nextToken();
+
+                EmployeeRecord rec = new EmployeeRecord(id, firstName, lastName);
+                this.insert(rec);
+            }
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    protected void deleteFromFile(String filename) throws ClassNotFoundException, InvalidRecordException, RemoteFailure {
+
+        try(BufferedReader reader = new BufferedReader(new FileReader(filename))){
+            String line;
+            while ((line = reader.readLine()) != null) {
+                int key = Integer.parseInt(line);
+
+                this.delete(key);
+            }
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
     }
 
     protected void insert(EmployeeRecord rec) throws ClassNotFoundException, InvalidRecordException, RemoteFailure, IOException {
